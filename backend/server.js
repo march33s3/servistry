1aa8d2399e79feaabe4c77887e9b7d578380e472
Marquis Davis <davismarquisj@gmail.com>
2024-07-21 14:02:36 +0000

    add routes for registration to Express app or server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const registrationRoutes = require('./routes/registration');
+
+
 const app = express();
 const cors = require("cors");
 
@@ -6,10 +11,10 @@ require("dotenv").config({ path: "./config.env"});
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
+app.use(bodyParser.json());
+//app.use(express.json());
+//app.use(require("./routes/record"));
 
-// Get MongoDB driver connection
-const dbo = require("./config/db");
+app.use('/api/registration', registrationRoutes)
 
 app.listen(port, () => console.log('Server is running on port: ${port}'));
