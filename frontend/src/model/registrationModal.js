58a184e83b1667d04d8e10664d9d7cb1878912d5
Marquis Davis <davismarquisj@gmail.com>
2024-07-21 13:38:41 +0000

    Update registrationModal schema

--- frontend/src/model/registrationModal.js
+++ frontend/src/model/registrationModal.js
@@ -1,11 +1,22 @@
-const registrationModal = {
-    promotionalOffersAndUpdates: false,
-    password: '',
-    userType: '',
-    eventID: '',
-    feeling: '',
-    emailAddress: '',
-    firstandLastName: '',
+const mongoose = require('mongoose');
 
-}
-export default registrationModal;
+const RegistrationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Registration', RegistrationSchema);
+
+
+
+//const registrationModal = {
+//    promotionalOffersAndUpdates: false,
+//    password: '',
+//    userType: '',
+//    eventID: '',
+//    feeling: '',
+//    emailAddress: '',
+//    firstandLastName: '',
+//}
+//export default registrationModal;
