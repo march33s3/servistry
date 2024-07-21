c0e1f7c8b92f84e2df27456bf310d9ff12c1ad8f
Marquis Davis <davismarquisj@gmail.com>
2024-07-21 14:17:05 +0000

    add registration POST route

---
+++ backend/routes/registration.js
@@ -0,0 +1,16 @@
+// routes/registration.js
+const express = require('express');
+const router = express.Router();
+const Registration = require('../models/registrationModal');
+
+// POST route to add a new user through registration
+router.post('/add', async (req, res) => {
+    const { firstandLastName, emailAddress, password, feeling, userType, eventID, promotionalOffersAndUpdates } = req.body;
+    try {
+      const newRegistration = new Registration({ firstandLastName, emailAddress, password, feeling, userType, eventID, promotionalOffersAndUpdates });
+      await newRegistration.save();
+      res.status(201).json(newRegistration);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
