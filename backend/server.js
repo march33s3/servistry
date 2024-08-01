4ed3331335eebe7099b95c1cc610e7c889a481be
Marquis Davis <davismarquisj@gmail.com>
2024-08-01 23:10:25 +0000

    reorganize declarations and add db connection

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const cors = require("cors");
 const bodyParser = require('body-parser');
+const connectDB = require('./config/db'); // Import the database connection module
 const registrationRoutes = require('./routes/registration');
 
 
 const app = express();
-const cors = require("cors");
-
 require("dotenv").config({ path: "./config.env"});
 const port = process.env.PORT || 5000;
 
+// Connect to the database
+connectDB();
+
+// Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-//app.use(require("./routes/record"));
 
+// Routes
 app.use('/api/registration', registrationRoutes)
 
+// Start server
 app.listen(port, () => console.log('Server is running on port: ${port}'));
