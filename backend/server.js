2c8173a6dc1f48020c155495034f25cc85fd78c4
Marquis Davis <davismarquisj@gmail.com>
2024-09-09 06:04:07 +0000

    update cors logic

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins (You can specify your frontend origin instead for better security)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(bodyParser.json());
 app.use(express.json());
 
